ed00f985e921f2c47e16d473e8e5c5e018d68bbd
Felipe  Campara Silva <fcamparasilva@gmail.com>
2020-02-12 17:41:11 -0300

    adinterfacesd

--- class/index.ts
+++ class/index.ts
@@ -55,6 +55,7 @@ class CarController {
 
 const car1 = new CarController('Ford', 'Ka', 185)
 
+// Super
 class Ferrari extends CarController {
   constructor(model: string, maxSpeed: number) {
     super('Ferrari', model, maxSpeed)
@@ -69,4 +70,75 @@ class Ferrari extends CarController {
   }
 }
 
-const f40 = new Ferrari('Ferrari', 'F40', 324)
+const f40 = new Ferrari('F40', 324)
+
+// Static
+class MathController {
+  static PI: number = 3.1416
+
+  static areaCircle(radius: number): number {
+    return this.PI * Math.pow(radius, 2)
+  }
+}
+
+MathController.areaCircle(4)
+
+// Abstract, can't initialize only extends
+abstract class CalcController {
+  protected result: number = 0
+
+  abstract execute(...numbers: number[]): void
+
+  get results(): number {
+    return this.result
+  }
+}
+
+class Sum extends CalcController {
+  execute(...numbers: number[]): void {
+    this.result = numbers.reduce((current, accumulator) => current + accumulator)
+  }
+}
+
+class Multiply extends CalcController {
+  execute (...numbers: number[]): void {
+    this.result = numbers.reduce((current, accumulator) => current * accumulator)
+  }
+}
+
+let c1: CalcController = new Sum()
+c1.execute(2, 3, 4, 5)
+
+c1 = new Multiply()
+c1.execute(2, 3, 4, 5)
+
+// Singleton
+class UnicController {
+  private static instance: UnicController = new UnicController
+
+  private constructor() {
+
+  }
+
+  static getInstance(): UnicController {
+    return UnicController.instance
+  }
+
+  now() {
+    return new Date()
+  }
+}
+
+UnicController.getInstance().now()
+
+// Only read
+class AirplaneController {
+  public readonly model: string
+
+  constructor(model: string, public readonly prefix: string) {
+    this.model = model
+  }
+}
+
+const turb = new AirplaneController('TU-114', 'PT-ABC')
+//turb.model = 'Boing'
